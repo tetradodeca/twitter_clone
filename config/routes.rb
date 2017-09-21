b73b64b1044612aeb76fd72019e15c0038435558
sha <urabummer4@hotmail.com>
2017-09-21 17:11:45 +0800

    crud for tweet and replies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "registrations"}
-
+  devise_for :users, :controllers => { registrations: "registrations" }
+  root 'tweets#index'
+  resources :tweets do
+    resources :replies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
--- db/migrate/20170919074437_devise_create_users.rb
+++ db/migrate/20170919074437_devise_create_users.rb
@@ -1,10 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
-      #username
-      t.string :name
-
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
