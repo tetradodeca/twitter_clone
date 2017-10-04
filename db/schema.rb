8e0c4408fc4d7b700a5b7f7a3c42c7d42d042af8
sha <urabummer4@hotmail.com>
2017-10-04 10:42:44 +0800

    data stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170926095105) do
 
   create_table "likes", force: :cascade do |t|
